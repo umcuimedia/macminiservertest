7f4e1e708088868500523bceb1e3a83ce9b475f4
Rens Wiebenga <renswiebenga@ma011373s-mac-mini.local>
2018-12-27 13:54:39 +0100

    first commit

---
+++ onservertest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  onservertest
+//
+//  Created by Rens Wiebenga on 27/12/2018.
+//  Copyright © 2018 umc utrecht. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
